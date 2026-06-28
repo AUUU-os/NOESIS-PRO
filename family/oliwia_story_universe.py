32f49801f452870008100e430c777909f6c853a8
shad9k <222700392+AUUU-os@users.noreply.github.com>
2026-06-28 19:46:43 +0100

    forge: add oliwia story universe

---
+++ family/oliwia_story_universe.py
@@ -0,0 +1,6 @@
+class OliwiaStoryUniverse:
+    def __init__(self):
+        self.worlds = ['Ocean Heart', 'Star Garden', 'Moon Forest']
+
+    def enter(self, world):
+        return f'Entering {world}'
